5dd70927a888653569b67e473ea0b381ef3ec92c
kishore <kishoregopi1540@gmail.com>
2024-08-21 17:38:40 +0530

    changes in url for production

--- crimeAnalysis/urls.py
+++ crimeAnalysis/urls.py
@@ -15,13 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('authentication.urls')),
     path('alarm',include('alarmprediction.urls')),
     path('prob', include('probprediction.urls')),
-    path('crimepred',include('crimepdesc.urls'))
+    path('crimepred',include('crimepdesc.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})
     # path('check',include('alarmprediction.urls'))
 ]
